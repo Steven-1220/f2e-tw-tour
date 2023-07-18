2dd1a2dd0f776510666a99212976240f3a9667fd
Steven Chan <richlife1220@gmail.com>
2023-07-18 22:27:49 +0800

    調整部屬到 github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/tw-tour/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/f2e-tw-tour/' : '/'
 })
