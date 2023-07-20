d51e4e492f19f30c5f1a3ff9fdd6a1a7e98346f9
Steven Chan <richlife1220@gmail.com>
2023-07-20 23:41:47 +0800

    加上 vue loading

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/css/index.css'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(VueAxios, axios)
 app.use(router)
+
+app.component('vue-loading', Loading)
 app.mount('#app')
+// VueLoading
