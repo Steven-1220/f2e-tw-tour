d19a66c6de87a2fbe48a9abaf0a7c25f238db198
Steven Chan <richlife1220@gmail.com>
2023-07-23 15:31:40 +0800

    刪除註解

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@
 <script>
 import getTdxAuthorize from '@/libs/TDXauthorize'
 // import TdxAuthorize from '@/libs/authorize'
-// import { Tooltip } from 'bootstrap/dist/js/bootstrap.esm.min.js'
-import { Tooltip } from 'bootstrap'
 
 export default {
   provide () {
@@ -31,8 +29,6 @@ export default {
   mounted () {
     // TdxAuthorize()
     getTdxAuthorize()
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    Array.from(tooltipTriggerList).map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
   }
 }
 </script>
