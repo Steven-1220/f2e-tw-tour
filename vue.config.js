bc21b0decab5008e4709fc8de6784a999fafddac
Steven Chan <richlife1220@gmail.com>
2023-07-27 10:43:13 +0800

    新增 favicon 和修改標題

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/f2e-tw-tour/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/f2e-tw-tour/' : '/',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '台灣景點旅遊網'
+        return args
+      })
+  }
 })
